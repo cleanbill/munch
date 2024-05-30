074df512722e3b3b595456a3c96317827832a5f2
Mick Carter <cick.marter@gmail.com>
2024-05-30 14:09:16 +0100

    clear token and re-enter

--- src/components/sync.tsx
+++ src/components/sync.tsx
@@ -1,8 +1,9 @@
 "use client"
 import { useLocalStorage } from "usehooks-ts";
 import { useEffect, useState } from "react";
-import { Eater, MUNCH, Dinner, SELECTED_MEAL, MealPlan, SELECTED_DATE_INDEX, MUNCH_BAK, VERSIONS_STAMP, API_KEY } from "../../types";
-import DinnerForm from "./dinnerForm";
+import { MUNCH, Dinner, MUNCH_BAK, VERSIONS_STAMP, API_KEY } from "../../types";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Sync = () => {
@@ -11,7 +12,7 @@ const Sync = () => {
     const [dinners, setDinners] = useLocalStorage(MUNCH, new Array<Dinner>());
     const [_back, setBack] = useLocalStorage(MUNCH_BAK, new Array<Dinner>());
     const [versionstamp, setVersionstamp] = useLocalStorage(VERSIONS_STAMP, 0);
-    const [token, _setToken] = useLocalStorage(API_KEY, "munch");
+    const [token, setToken] = useLocalStorage(API_KEY, "munch");
 
     useEffect(() => {
         setMounted(true);
@@ -24,15 +25,34 @@ const Sync = () => {
             headers: { "Content-Type": "application/json" },
         };
         const response = await fetch(URL, requestOptions);
-        const data = await response.json();
-
-        return data;
+        if (response.status != 200) {
+            toast.error('Failed to Sync data', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light"
+            });
+            return;
+        }
+        try {
+            const data = await response.json();
+            return data;
+        } catch (err) {
+            console.error('Cannot parse data', err);
+            toast("Sync failed - parsing problem");
+        }
     }
 
     const load = async () => {
 
         const data = await getData();
-
+        if (!data) {
+            return;
+        }
         // Back it up...
         setBack([...dinners]);
         setVersionstamp(data.versionstamp);
@@ -45,13 +65,23 @@ const Sync = () => {
         }
         const data = await getData();
         if (data.versionstamp != versionstamp) {
+            toast.error('Cannot Send - out of sync', {
+                position: "top-center",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light"
+            });
             throw Error("Out of sync");
         }
     }
 
     const save = async () => {
         checkVersion();
-        const URL = 'sync';
+        const URL = 'sync/' + token;
         const data = {
             token,
             dinners,
@@ -65,13 +95,34 @@ const Sync = () => {
         await fetch(URL, requestOptions);
     }
 
+    const hasToken = token != 'munch';
+    const noToken = !hasToken;
+
+    const updateToken = () => {
+        const el = document.getElementById("token-input") as HTMLInputElement;
+        setToken(el.value);
+    }
+
+    const clearToken = () => {
+        setToken("munch");
+    }
+
     return (
         <div >
-            {mounted && <div className='grid grid-cols-2'>
-                <button className="w-10 text-red-500 hover:bg-blue-200 focus:outline-none focus:ring hover:pr-0 focus:ring-yellow-300 text-xs rounded-xl h-5 float-start" onClick={load} >Sync</button>
-                <button className="w-10 text-green-500 hover:bg-blue-200 focus:outline-none focus:ring hover:pr-0 focus:ring-yellow-300 text-xs rounded-xl h-5 place-self-end" onClick={save} >Send</button>
+
+            {mounted && noToken && <div>
+                <input id="token-input" className="w-4/5 bg-sky-200 text-left" placeholder="Whats the token"></input><button className="float-right w-10 text-red-500 bg-yellow-100 hover:bg-blue-200 focus:outline-none focus:ring hover:pr-0 focus:ring-yellow-300 text-xs rounded-xl h-5" onClick={updateToken}>post</button>
             </div>}
-        </div>
+
+            {mounted && hasToken && <div>
+                < ToastContainer />
+                <div className='grid grid-cols-3'>
+                    <button className="w-10 text-red-500 hover:bg-blue-200 focus:outline-none focus:ring hover:pr-0 focus:ring-yellow-300 text-xs rounded-xl h-5 float-start" onClick={load} >Sync</button>
+                    <button className="w-10 text-red-500 hover:bg-blue-200 focus:outline-none focus:ring hover:pr-0 focus:ring-yellow-300 text-xs rounded-xl h-5 place-self-center" onClick={clearToken} >clear</button>
+                    <button className="w-10 text-green-500 hover:bg-blue-200 focus:outline-none focus:ring hover:pr-0 focus:ring-yellow-300 text-xs rounded-xl h-5 place-self-end" onClick={save} >Send</button>
+                </div>
+            </div>}
+        </div >
     )
 }
 
